5498e36a79a53c19ffe61c129a27d1ec0e5c264a
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2020-10-18 21:41:21 +0500

    completed navbar

--- src/components/MidHeader/MidHeader.js
+++ src/components/MidHeader/MidHeader.js
@@ -4,17 +4,8 @@ import "./MidHeader.css";
 
 const MidHeader = () => {
   return (
-    <div className="midheader mt-40">
-      <div className="container">
-        <div class="row">
-          <div class="column side heading">Umer</div>
-          <div class="column side">As Player</div>
-          <div class="column middle">As Referee</div>
-          <div class="column side">
-            <DropDown />
-          </div>
-        </div>
-      </div>
+    <div className="dropdown">
+      <DropDown />
     </div>
   );
 };
